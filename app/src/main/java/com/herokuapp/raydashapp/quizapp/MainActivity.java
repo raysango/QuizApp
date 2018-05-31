eeb4d541a3ba98005c52e8b050bfc32b1fee2512
Raymond Morcos <remongeorge@live.com>
2018-05-31 05:47:55 -0700

    Adds score message as a toast

--- app/src/main/java/com/herokuapp/raydashapp/quizapp/MainActivity.java
+++ app/src/main/java/com/herokuapp/raydashapp/quizapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.herokuapp.raydashapp.quizapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +9,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -88,8 +90,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void CalculateTotalScore(){
-            TextView Score = findViewById(R.id.score);
-            Score.setText("Your Final Score is " + score);
+            Context context = getApplicationContext();
+            CharSequence text = "Your Final Score is " + score;
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
             score = 0;
         }
 
